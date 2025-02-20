6af1aa5a0fa784045b2431957e83b999e9b9bc43
Pedro Henrique <167721170+eupedrobarbosa03@users.noreply.github.com>
2025-02-20 15:11:45 +0000

    update

--- TEMA_1/SUPER_TRUFO_NIVEL_AVENTUREIRO/main.c
+++ TEMA_1/SUPER_TRUFO_NIVEL_AVENTUREIRO/main.c
@@ -2,7 +2,7 @@
 
 int main(){
     char letra_estado_carta1, letra_estado_carta2;
-    char codigo_estado_carta1[4], codigo_estado_carta2[4];
+    char codigo_carta1[4], codigo_carta2[4];
     char nome_cidade_carta1[16], nome_cidade_carta2[16];
     int populacao_carta1, populacao_carta2;
     float area_carta1, area_carta2;
@@ -16,7 +16,7 @@ int main(){
     scanf("%c", &letra_estado_carta1);
 
     printf("Digite a letra do Estado + um número de 01 a 04. Exemplo(A01): ");
-    scanf("%s", &codigo_estado_carta1);
+    scanf("%s", &codigo_carta1);
 
     printf("Digite o nome da Cidade: ");
     scanf(" %[^\n]", nome_cidade_carta1);
@@ -38,7 +38,7 @@ int main(){
     float densidade_populacional_carta1 = (float) populacao_carta1 / area_carta1;
     float pib_por_capita_carta1 = (float) (pib_carta1 * 1000000000) / populacao_carta1;
 
-    printf("Letra do Estado: %c\nCódigo do Estado: %s\nCidade: %s\nPopulação: %d\nÁrea: %.2fkm²\nPIB: %.2f\nQuantidade de Pontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB por Capita: %.2f reais.\n", letra_estado_carta1, codigo_estado_carta1, nome_cidade_carta1, populacao_carta1, area_carta1, pib_carta1, qtd_pontos_turisticos_carta1, densidade_populacional_carta1, pib_por_capita_carta1);
+    printf("Letra do Estado: %c\nCódigo da Carta: %s\nCidade: %s\nPopulação: %d\nÁrea: %.2fkm²\nPIB: %.2f\nQuantidade de Pontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB por Capita: %.2f reais.\n", letra_estado_carta1, codigo_carta1, nome_cidade_carta1, populacao_carta1, area_carta1, pib_carta1, qtd_pontos_turisticos_carta1, densidade_populacional_carta1, pib_por_capita_carta1);
 
     printf("\nDADOS CARTA 2\n");
 
@@ -48,7 +48,7 @@ int main(){
     scanf("%c", &letra_estado_carta2);
 
     printf("Digite a letra do Estado + um número de 01 a 04. Exemplo(A01): ");
-    scanf("%s", &codigo_estado_carta2);
+    scanf("%s", &codigo_carta2);
 
     printf("Digite o nome da Cidade: ");
     scanf(" %[^\n]", nome_cidade_carta2);
@@ -70,7 +70,7 @@ int main(){
     float densidade_populacional_carta2 = (float) populacao_carta2 / area_carta2;
     float pib_por_capita_carta2 = (float) (pib_carta2 * 1000000000) / populacao_carta2;
 
-    printf("Letra do Estado: %c\nCódigo do Estado: %s\nCidade: %s\nPopulação: %d\nÁrea: %.2fkm²\nPIB: %.2f\nQuantidade de Pontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB por Capita: %.2f reais.\n", letra_estado_carta2, codigo_estado_carta2, nome_cidade_carta2, populacao_carta2, area_carta2, pib_carta2, qtd_pontos_turisticos_carta2, densidade_populacional_carta2, pib_por_capita_carta2);
+    printf("Letra do Estado: %c\nCódigo da Carta: %s\nCidade: %s\nPopulação: %d\nÁrea: %.2fkm²\nPIB: %.2f\nQuantidade de Pontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB por Capita: %.2f reais.", letra_estado_carta2, codigo_carta2, nome_cidade_carta2, populacao_carta2, area_carta2, pib_carta2, qtd_pontos_turisticos_carta2, densidade_populacional_carta2, pib_por_capita_carta2);
 
     
     return 0;
