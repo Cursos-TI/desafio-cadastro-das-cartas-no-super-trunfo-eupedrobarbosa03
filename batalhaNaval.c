7c943dbccca27e2aa3d68ff316d13988d8a618e9
Pedro Henrique <167721170+eupedrobarbosa03@users.noreply.github.com>
2025-03-12 22:02:45 +0000

    desafio batalha naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,46 +1,58 @@
 #include <stdio.h>
 
-int main(){
-    //Tabuleiro 10x10
-    int tabuleiro[10][10] = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
+#define LINHAS_TABULEIRO 10
+#define COLUNAS_TABULEIRO 10
+#define TAMANHO_MAX_NAVIO 3
 
-    int navio1 = 3;
-    int navio2 = 3;
-    int navio3 = 3;
-    int navio4 = 3;
+int main() {
 
-    tabuleiro[9][0] = navio1;
-    tabuleiro[9][1] = navio1;
-    tabuleiro[9][2] = navio1;
+    //Tabuleiro 10x10.
+    int tabuleiro[LINHAS_TABULEIRO][COLUNAS_TABULEIRO];
 
-    tabuleiro[7][8] = navio2;
-    tabuleiro[6][8] = navio2;
-    tabuleiro[5][8] = navio2;
+    //Tabuleiro 10x10 criado com laços for alinhados.
+    for(int i = 0; i < LINHAS_TABULEIRO; i++) {
+        for(int j = 0; j < COLUNAS_TABULEIRO; j++) {
+            tabuleiro[i][j] = 0;
+        }
+    }
 
-    tabuleiro[0][0] = navio3;
-    tabuleiro[1][1] = navio3;
-    tabuleiro[2][2] = navio3;
+    //Os navios são representando pelo número 3.
+    int representar_navios = 3;
 
-    tabuleiro[0][9] = navio4;
-    tabuleiro[1][8] = navio4;
-    tabuleiro[2][7] = navio4;
-    
-    for(int i = 0; i < 10; i++) {
-        for(int j = 0; j < 10; j++){
+    //Posições dos navios.
+    int pos_horizontal, pos_vertical, pos_diagonal;
+
+    //Navios e posições
+    int navio1;
+    int navio2;
+    int navio3;
+    int navio4;
+
+    //Navio 1
+    for(pos_horizontal = 3; pos_horizontal < 6; pos_horizontal++) {
+        tabuleiro[4][pos_horizontal] = representar_navios;
+    }
+
+    //Navio 2
+    for(pos_vertical = 0; pos_vertical < 3; pos_vertical++) {
+        tabuleiro[pos_vertical][4] = representar_navios;
+    }
+
+    //Navio 3
+    for(pos_diagonal = 0; pos_diagonal < 3; pos_diagonal++) {
+        tabuleiro[pos_diagonal][pos_diagonal] = representar_navios;
+    }
+
+    //Navio 4
+    for(pos_diagonal = 7; pos_diagonal < 10; pos_diagonal++) {
+        tabuleiro[pos_diagonal][pos_diagonal] = representar_navios;
+    }
+
+    for(int i = 0; i < LINHAS_TABULEIRO; i++) {
+        for(int j = 0; j < COLUNAS_TABULEIRO; j++){
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
-    
+
 }
